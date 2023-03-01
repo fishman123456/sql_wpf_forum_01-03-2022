7466650f660a6fce923a11f96761d44d25e110b6
denis <106389581+fishman123456@users.noreply.github.com>
2023-03-01 19:26:47 +0700

    готовы таблицы

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.SqlServer;
+using Microsoft.Data.SqlClient;
+using System.Data.SqlClient;
 
 namespace sql_wpf_forum_01_03_2022
 {
@@ -20,9 +23,18 @@ namespace sql_wpf_forum_01_03_2022
     /// </summary>
     public partial class MainWindow : Window
     {
+       const string sqlSource = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Library;
+Integrated Security=True";
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Load2_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 w1 = new Window1();
+            w1.Show();
+        }
     }
+
 }
---
+++ запрос на создание таблицы.sql
@@ -0,0 +1,12 @@
+﻿--use academy_top
+
+--create table students
+--(
+--id int not null identity(1,1) primary key,
+--studName nvarchar(50) not null,
+--studFname nvarchar (50) not null,
+--studAge int not null,
+--gropeName nvarchar (50) not null,
+--grants int 
+--)
+--alter database scoped configuration set identity_cache=off 
