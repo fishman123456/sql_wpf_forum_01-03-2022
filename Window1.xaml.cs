3a05b317c612e9fd9bcb9a5c2c9f1394b39c25b9
denis <106389581+fishman123456@users.noreply.github.com>
2023-03-01 20:28:02 +0700

    создали безопасный ввод

--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +14,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Microsoft.Identity.Client.Extensions.Msal;
 
 namespace sql_wpf_forum_01_03_2022
 {
@@ -19,9 +23,60 @@ namespace sql_wpf_forum_01_03_2022
     /// </summary>
     public partial class Window1 : Window
     {
+        const string sqlSource = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog = academy_top;
+Integrated Security=True";
+
         public Window1()
         {
             InitializeComponent();
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+            
+            SqlConnection con = new SqlConnection();
+            
+            //описываем соединение 
+            con = new SqlConnection(sqlSource);
+
+          
+
+            //открываем соединение
+            con.Open();
+                string query = "insert into students (studName, studFname, studAge, gropeName, grants" +
+                    "@studName, @studFname, @studAge, @gropeName, @grants";
+
+                //создаем комманду на соединение
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+              
+            }
+
+        }
+        private SqlParameter[] GetSqlParameters() 
+        {
+        SqlParameter studName = new SqlParameter("@studName",SqlDbType.NVarChar);
+            studName.Value = name_t.Text;
+            SqlParameter studSone = new SqlParameter("@studFname", SqlDbType.NVarChar);
+            studName.Value = so_name_t.Text;
+            SqlParameter studAge = new SqlParameter("@studAge", SqlDbType.Int);
+            studName.Value = age_t.Text;
+            SqlParameter studGroup = new SqlParameter("@gropeName", SqlDbType.NVarChar);
+            studName.Value = group_t.Text;
+            SqlParameter studGrant = new SqlParameter("@grants", SqlDbType.Int);
+            studName.Value = Int32.Parse (grant_t.Text);
+            return new SqlParameter[] { studName,studSone, studAge, studGroup, studGrant};
+                 
+     
+        
         }
     }
 }
